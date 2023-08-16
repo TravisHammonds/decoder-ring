096f270a1fbc8d63c2790d38028fb17306af74f6
Travis Hammonds <travishammonds2@gmail.com>
2023-08-16 11:09:41 -0400

    refactored caesar and added 9 new test for it

--- Project_Decoder_ring_1/src/caesar.js
+++ Project_Decoder_ring_1/src/caesar.js
@@ -1,103 +1,37 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
-
   function caesar(input, shift, encode = true) {
-    // your solution code here
-    const alphabet = new Map([
-      ["a", 1],
-      ["b", 2],
-      ["c", 3],
-      ["d", 4],
-      ["e", 5],
-      ["f", 6],
-      ["g", 7],
-      ["h", 8],
-      ["i", 9],
-      ["j", 10],
-      ["k", 11],
-      ["l", 12],
-      ["m", 13],
-      ["n", 14],
-      ["o", 15],
-      ["p", 16],
-      ["q", 17],
-      ["r", 18],
-      ["s", 19],
-      ["t", 20],
-      ["u", 21],
-      ["v", 22],
-      ["w", 23],
-      ["x", 24],
-      ["y", 25],
-      ["z", 26],
-    ]);
-    //less than -25, or greater than 25, the function should return false.
-    if (
-      !input ||
-      shift < -25 ||
-      shift > 25 ||
-      shift === 0
-    ) {
+    if (shift === 0 || shift < -25 || shift > 25 || typeof(shift) !== 'number') {
       return false;
     }
-    let modifiedInput = [];
-    // loop through each characters
+
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    let modifiedInput = "";
+
     for (let i = 0; i < input.length; i++) {
-      // set the current character for the iteration
       let char = input[i].toLowerCase();
-      // if the character is not a letter, keep it in place
-      if (!alphabet.has(char)) modifiedInput[i] = char;
-      else {
-        //swap the char with the amount shifted
-        //find the position of the current character
-        let charPosition = alphabet.get(char);
-        //find the position of the next character
-        let newPosition;
+      if (!alphabet.includes(char)) {
+        modifiedInput += char;
+      } else {
+        let charIndex = alphabet.indexOf(char);
+        let newIndex;
         if (encode) {
-          let tempPosition = charPosition + shift;
-          //if temp is within the alphabet
-          if (1 <= tempPosition && tempPosition <= 26)
-            newPosition = tempPosition;
-          //if temp wraps the end of the alphabet
-          else if (tempPosition > 26) newPosition = tempPosition - 26;
-          // if temp wraps around the front of the alphabet
-          else if (tempPosition < 1) newPosition = 26 + tempPosition;
+          newIndex = (charIndex + shift + 26) % 26;
         } else {
-          let tempPosition = charPosition - shift;
-          console.log(char, charPosition, tempPosition)
-          //if temp is within the alphabet
-          if (1 <= tempPosition && tempPosition <= 26)
-            newPosition = tempPosition;
-          //if temp wraps the end of the alphabet
-          else if (tempPosition > 26) newPosition = tempPosition - 26;
-          // if temp wraps around the front of the alphabet
-          else if (tempPosition < 1) newPosition = 26 + tempPosition;
+          newIndex = (charIndex - shift + 26) % 26;
         }
-        //find the character at the new position
-        let newChar;
-        for (const [key, value] of alphabet.entries()) {
-          if (value === newPosition) {
-            newChar = key;
-            break;
-          }
-        }
-        //add that character to the modified input at current index
-        modifiedInput[i] = newChar;
+        modifiedInput += alphabet[newIndex];
       }
     }
-    console.log(modifiedInput.join(""));
-    return modifiedInput.join("");
+
+    return modifiedInput;
   }
+
   return {
     caesar,
   };
 })();
 
-caesarModule.caesar("drkxupevvi", 10, false);
+console.log(caesarModule.caesar("hello", 7, true))
 
 module.exports = { caesar: caesarModule.caesar };
--- Project_Decoder_ring_1/test/caesar.test.js
+++ Project_Decoder_ring_1/test/caesar.test.js
@@ -1,2 +1,62 @@
 // Write your tests here!
 
+const expect = require("chai").expect;
+const { caesar } = require('../src/caesar');
+
+describe('caesar', () => {
+  it('should return false if shift is not a valid integer', () => {
+    const actual = caesar("hello", "3uq345h", true)
+    const expected = false;
+    expect(expected).to.equal(actual);
+  });
+
+  it('should return false if shift is 0', () => {
+    const actual = caesar("hello", 0, true)
+    const expected = false;
+    expect(expected).to.equal(actual);
+  });
+
+  it('should return false if shift is less than -25', () => {
+    const actual = caesar("hello", -26, true)
+    const expected = false;
+    expect(expected).to.equal(actual);
+  });
+
+  it('should return false if shift is greater than 25', () => {
+    const actual = caesar("hello", 26, true)
+    const expected = false;
+    expect(expected).to.equal(actual);
+  });
+
+  it('should maintain spaces and non-alphabetic symbols', () => {
+    const actual = caesar("Hello There!", 4, true)
+    const expected = "lipps xlivi!";
+    expect(expected).to.equal(actual);
+  });
+
+  it('should ignore capital letters', () => {
+    const actual = caesar("HELLO", 4, true)
+    const expected = "lipps";
+    expect(expected).to.equal(actual);
+  });
+
+  it('should wrap around the alphabet', () => {
+    const actual = caesar("hello", 23, true)
+    const expected = "ebiil";
+    expect(expected).to.equal(actual);
+  });
+
+  it('should encode text to the right', () => {
+    const actual = caesar("hello", 7, true)
+    const expected = "olssv";
+    expect(expected).to.equal(actual);
+  });
+   
+
+  it('should decode text to the left', () => {
+    const actual = caesar("olssv", 7, false)
+    const expected = "hello";
+    expect(expected).to.equal(actual);
+  });
+    
+});
